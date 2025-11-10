e0af4aef51a6be655999105e6c93c18e634481e2
Tonnie <vontonnie@gmail.com>
2025-11-10 17:42:55 +0300

    Add custom shape definitions for Compose UI theme

---
+++ app/src/main/java/com/tonyxlab/lazypizza/ui/theme/Shapes.kt
@@ -0,0 +1,34 @@
+package com.tonyxlab.lazypizza.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
+
+val customMaterialShapes = Shapes(
+        // Pre-Defined M3 Shapes
+        extraSmall = RoundedCornerShape(4.dp),
+        small = RoundedCornerShape(8.dp),
+        medium = RoundedCornerShape(12.dp),
+        large = RoundedCornerShape(16.dp),
+
+        // Extra-Large Override
+        extraLarge = RoundedCornerShape(20.dp)
+
+)
+
+object ExtendedShapes {
+
+    val RoundedCornerShape100 = RoundedCornerShape(100.dp)
+
+    val HorizontalRoundedCornerShape16 = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp)
+}
+
+val Shapes.RoundedCornerShape100
+    @Composable
+    get() = ExtendedShapes.RoundedCornerShape100
+
+
+val Shapes.HorizontalRoundedCornerShape16
+    @Composable
+    get() = ExtendedShapes.HorizontalRoundedCornerShape16
