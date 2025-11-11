8b1392f60671ebd9766c0078ff0a2c8a86a2bede
Tonnie <vontonnie@gmail.com>
2025-11-11 07:57:00 +0300

    Integrate Koin DI

--- app/src/main/java/com/tonyxlab/lazypizza/LazyPizzaApp.kt
+++ app/src/main/java/com/tonyxlab/lazypizza/LazyPizzaApp.kt
@@ -1,11 +1,25 @@
 package com.tonyxlab.lazypizza
 
 import android.app.Application
+import com.tonyxlab.lazypizza.di.viewModelModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 import timber.log.Timber
 
 class LazyPizzaApp: Application() {
     override fun onCreate() {
         super.onCreate()
-        Timber.plant(Timber.DebugTree())
+
+        if (BuildConfig.DEBUG){
+            Timber.plant(Timber.DebugTree())
+        }
+
+        startKoin {
+
+            androidContext(androidContext = this@LazyPizzaApp)
+            modules(
+                    listOf(viewModelModule,)
+            )
+        }
     }
 }
---
+++ app/src/main/java/com/tonyxlab/lazypizza/di/AppModule.kt
@@ -0,0 +1,7 @@
+package com.tonyxlab.lazypizza.di
+
+import com.tonyxlab.lazypizza.presentation.screens.home.HomeViewModel
+import org.koin.core.module.dsl.viewModelOf
+import org.koin.dsl.module
+
+val viewModelModule = module { viewModelOf(::HomeViewModel) }
