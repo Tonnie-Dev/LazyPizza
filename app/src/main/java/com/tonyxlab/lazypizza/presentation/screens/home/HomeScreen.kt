d850d1f02a5428c43f440c5183eebbf83e605bb8
Tonnie <vontonnie@gmail.com>
2025-11-11 18:22:25 +0300

    Add Image Banner

--- app/src/main/java/com/tonyxlab/lazypizza/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/tonyxlab/lazypizza/presentation/screens/home/HomeScreen.kt
@@ -1,21 +1,32 @@
 package com.tonyxlab.lazypizza.presentation.screens.home
 
 import android.content.Intent
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.core.net.toUri
+import com.tonyxlab.lazypizza.R
 import com.tonyxlab.lazypizza.presentation.core.base.BaseContentLayout
 import com.tonyxlab.lazypizza.presentation.core.components.AppTopBar
 import com.tonyxlab.lazypizza.presentation.core.utils.spacing
 import com.tonyxlab.lazypizza.presentation.screens.home.handling.HomeActionEvent
 import com.tonyxlab.lazypizza.presentation.screens.home.handling.HomeUiEvent
+import com.tonyxlab.lazypizza.presentation.screens.home.handling.HomeUiState
+import com.tonyxlab.lazypizza.presentation.theme.LazyPizzaTheme
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
@@ -26,11 +37,11 @@ fun HomeScreen(
     val uiState by viewModel.uiState.collectAsState()
     val context = LocalContext.current
     BaseContentLayout(
-            modifier = modifier.padding(MaterialTheme.spacing.spaceMedium),
+            modifier = modifier,
             viewModel = viewModel,
             topBar = {
                 AppTopBar(
-                        modifier = Modifier.padding(MaterialTheme.spacing.spaceMedium),
+                        modifier = Modifier.padding(horizontal = MaterialTheme.spacing.spaceMedium),
                         phoneNumber = uiState.phoneNumber, onCallClick = {
                     viewModel.onEvent(HomeUiEvent.PlaceCall)
                 }
@@ -51,9 +62,55 @@ fun HomeScreen(
                 }
             },
             containerColor = MaterialTheme.colorScheme.background
-    ) {
-        Box(modifier = Modifier.fillMaxSize()) {
+    ) { uiState ->
+        HomeScreenContent(
+                uiState = uiState,
+                onEvent = viewModel::onEvent
+        )
+    }
+}
 
+@Composable
+private fun HomeScreenContent(
+    uiState: HomeUiState,
+    onEvent: (HomeUiEvent) -> Unit,
+    modifier: Modifier = Modifier
+) {
+
+    Column(
+            modifier = modifier
+                    .fillMaxSize()
+                    .padding(horizontal = MaterialTheme.spacing.spaceMedium)
+    ) {
+
+       // AppTopBar(phoneNumber = "+254 723 445 818", onCallClick = { onEvent(HomeUiEvent.PlaceCall)})
+        Image(
+                modifier = Modifier
+                        .clip(MaterialTheme.shapes.small)
+                        .fillMaxWidth(),
+                painter = painterResource(R.drawable.banner_small),
+                contentDescription = stringResource(R.string.cds_text_banner),
+                contentScale = ContentScale.Crop
+        )
+
+    }
+}
+
+@PreviewLightDark
+@Composable
+private fun HomeScreenContent_Preview() {
+
+    LazyPizzaTheme {
+
+        Column(
+                modifier = Modifier
+                        .background(MaterialTheme.colorScheme.background)
+                        .fillMaxSize()
+        ) {
+            HomeScreenContent(
+                    uiState = HomeUiState(),
+                    onEvent = {}
+            )
         }
     }
 }
