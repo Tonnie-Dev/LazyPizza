157692126103cfe1d660a7d2e66e399faad4f66e
Tonnie <vontonnie@gmail.com>
2025-11-12 13:12:46 +0300

    Add Pizza data model class

---
+++ app/src/main/java/com/tonyxlab/lazypizza/domain/model/FoodItem.kt
@@ -0,0 +1,21 @@
+package com.tonyxlab.lazypizza.domain.model
+
+data class Pizza(
+    val id: Long,
+    val name: String,
+    val toppings: List<String>,
+    val price: Double,
+    val imageUrl: String,
+    val description: String? = null,
+    val category: Category
+)
+
+enum class Category(
+    val categoryName: String,
+    val folderPath: String
+) {
+    PIZZA(categoryName = "Pizza", folderPath = "pizza"),
+    DRINKS(categoryName = "Drinks", folderPath = "drinks"),
+    SAUCE(categoryName = "Sauces", folderPath = "sauce"),
+    ICE_CREAM(categoryName = "Ice Cream", folderPath = "icecream")
+}
