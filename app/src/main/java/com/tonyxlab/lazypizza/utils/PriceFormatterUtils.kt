97b7209aa319cae9205e5cf0c6550180c721e991
Tonnie <vontonnie@gmail.com>
2025-11-15 23:33:54 +0300

    Create PriceFormatterUtils.kt

---
+++ app/src/main/java/com/tonyxlab/lazypizza/utils/PriceFormatterUtils.kt
@@ -0,0 +1,9 @@
+package com.tonyxlab.lazypizza.utils
+
+fun Double.toPrice(): String {
+    return if (this % 1.0 == 0.0) {
+        "$${this.toInt()}"
+    } else {
+        "$${"%.2f".format(this)}"
+    }
+}
