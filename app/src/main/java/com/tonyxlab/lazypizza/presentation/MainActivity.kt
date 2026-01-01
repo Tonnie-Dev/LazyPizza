ede2eebe3de02b0872ff296a2cb5460ef75b1e2a
Tonnie <vontonnie@gmail.com>
2026-01-01 09:15:57 +0300

    Customize navigation bar color with edge-to-edge

--- app/src/main/java/com/tonyxlab/lazypizza/presentation/MainActivity.kt
+++ app/src/main/java/com/tonyxlab/lazypizza/presentation/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.tonyxlab.lazypizza.presentation
 
+import android.graphics.Color.toArgb
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.toArgb
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.tonyxlab.lazypizza.navigation.NavRoot
+import com.tonyxlab.lazypizza.presentation.theme.Background
 import com.tonyxlab.lazypizza.presentation.theme.LazyPizzaTheme
 
 class MainActivity : ComponentActivity() {
@@ -16,13 +21,15 @@ class MainActivity : ComponentActivity() {
         installSplashScreen().apply {
             setKeepOnScreenCondition { false }
         }
-
-        enableEdgeToEdge()
-
+        enableEdgeToEdge(
+                navigationBarStyle = SystemBarStyle.light(
+                        scrim =Background.toArgb(),
+                        darkScrim = Background.toArgb()
+                )
+        )
         setContent {
             LazyPizzaTheme {
                 Box(
-
                         contentAlignment = Alignment.Center
                 ) {
                     NavRoot()
